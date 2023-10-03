a02987ca8f9f4682c26ab2660dccc647d55e1320
ThLuan2809 <luanntph26376@fpt.edu.vn>
2023-10-03 08:36:04 +0700

    get phong ban theo id

--- controllers/PhongBanController.js
+++ controllers/PhongBanController.js
@@ -21,11 +21,31 @@ exports.getListPhongBan = async (req, res) => {
   }
 };
 
+exports.getPhongBanByID = async (req, res) => {
+  try {
+    await sql.connect(dbConfig);
+
+    const procName = "GetPhongBanByID";
+
+    const request = new sql.Request();
+
+    const result = await request.execute(procName);
+
+    res.json(result.recordset);
+
+    sql.close();
+  } catch (err) {
+    res.status(500).send("Lỗi khi lấy dữ liệu");
+    console.log(err);
+    sql.close();
+  }
+};
+
 exports.insertPhongBan = async (req, res) => {
   await sql.connect(dbConfig);
   var phongBan = {
     TenPhongBan: req.body.TenPhongBan,
-    ArrMaVT: req.body.ArrMaVT
+    ArrMaVT: req.body.ArrMaVT,
   };
 
   const query = `insert into PhongBan(TenPhongBan, ArrMaVT) values (N'${phongBan.TenPhongBan}', '${phongBan.ArrMaVT}')`;
@@ -48,10 +68,25 @@ exports.updatePhongBan = async (req, res) => {
   var phongBan = {
     MaPB: req.params.MaPB,
     TenPhongBan: req.body.TenPhongBan,
-    ArrMaVT: req.body.ArrMaVT
+    ArrMaVT: req.body.ArrMaVT,
   };
 
-  const query = `update PhongBan set TenPhongBan = N'${phongBan.TenPhongBan}', ArrMaVT = '${phongBan.ArrMaVT}'
+  const procName = "GetPhongBanByID";
+
+  const request = new sql.Request();
+  request.input("MaPB", sql.Int, phongBan.MaPB);
+
+  const result = await request.execute(procName);
+
+  const query = `update PhongBan set TenPhongBan = N'${
+    phongBan.TenPhongBan == undefined
+      ? result.recordset[0].TenPhongBan
+      : phongBan.TenPhongBan
+  }', ArrMaVT = '${
+    phongBan.ArrMaVT == undefined
+      ? result.recordset[0].ArrMaVT
+      : phongBan.ArrMaVT
+  }'
                   where MaPB = ${phongBan.MaPB}`;
 
   sql.query(query, (err, rows) => {
